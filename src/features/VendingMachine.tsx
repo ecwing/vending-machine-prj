cf40825961c8ba0b07ceeb379e0882d11cb2897c
Eric Wing <wingman@Erics-MacBook-Pro.local>
2025-05-19 16:17:42 -0400

    VMP: addition of vending machine logo image, logic to show and hide in specific area on UI with associated CSS, css changes for @media max-width, and banner image stlying

--- src/features/VendingMachine.tsx
+++ src/features/VendingMachine.tsx
@@ -4,6 +4,7 @@ import ProductSlot from '../components/ProductSlot';
 import DisplayScreen from '../components/DisplayScreen';
 import ControlPanel from '../components/ControlPanel';
 import { COINS, DRINK_IMAGES } from '../constants';
+import bannerLogo from '../assets/logo.png';
 
 import { useVendingMachine } from '../hooks/useVendingMachine';
 
@@ -24,60 +25,78 @@ const VendingMachine: React.FC = () => {
   } = useVendingMachine();
 
   return (
-    <div className="vendingMachineWrapper">
-      <h1 className="screen-reader-only">Crisp Vending Machine</h1>
-      <div>
-        <DisplayScreen
-          message={message}
-          balance={balance}
-          remainingBalanceOnItem={remainingBalanceOnItem}
-          selectedProduct={selectedProduct}
-        />
+    <div>
+      <div className="vendingMachineWrapper">
+        <h1 className="screen-reader-only">Crisp Vending Machine</h1>
+        <div></div>
+        <div>
+          <img
+            src={bannerLogo}
+            alt={'Crisp Vending Machine Banner'}
+            className="banner banner-mobile"
+          />
 
-        <h3>Insert Coins</h3>
-        <div className="coinContainer">
-          {COINS.map(coin => (
-            <div className="coinItem">
-              <CoinButton
-                key={coin}
-                type={coin}
-                onClick={() => handleDeposit(coin)}
-                currency={currency}
+          <DisplayScreen
+            message={message}
+            balance={balance}
+            remainingBalanceOnItem={remainingBalanceOnItem}
+            selectedProduct={selectedProduct}
+          />
+
+          <h3>Insert Coins</h3>
+          <div className="coinContainer">
+            {COINS.map(coin => (
+              <div className="coinItem">
+                <CoinButton
+                  key={coin}
+                  type={coin}
+                  onClick={() => handleDeposit(coin)}
+                  currency={currency}
+                />
+              </div>
+            ))}
+          </div>
+          {machineState.products.map(p => (
+            <div className="productItem">
+              <ProductSlot
+                key={p.name}
+                product={p}
+                onClick={() => handleSelectProduct(p)}
               />
             </div>
           ))}
+          <ControlPanel onPurchase={handlePurchase} onCancel={handleCancel} />
         </div>
-        {machineState.products.map(p => (
-          <div className="productItem">
-            <ProductSlot
-              key={p.name}
-              product={p}
-              onClick={() => handleSelectProduct(p)}
-            />
-          </div>
-        ))}
-        <ControlPanel onPurchase={handlePurchase} onCancel={handleCancel} />
-      </div>
 
-      <div className="productGrid">
-        {machineState.products.map(p => (
-          <div key={p.name} className="productItem">
-            <div
-              className={`productImageWrapper ${p.stock === 0 ? 'soldOut' : ''}`}
-            >
-              <img
-                src={DRINK_IMAGES[p.key]}
-                alt={`${p.name}`}
-                className="productImage"
-              />
-              {p.stock === 0 && <div className="soldOutOverlay">Sold Out</div>}
-            </div>
+        <div>
+          <img
+            src={bannerLogo}
+            alt="Crisp Vending Machine Banner"
+            className="banner banner-desktop"
+          />
+          <div className="productGrid">
+            {machineState.products.map(p => (
+              <div key={p.name} className="productItem">
+                <div
+                  className={`productImageWrapper ${p.stock === 0 ? 'soldOut' : ''}`}
+                >
+                  <img
+                    src={DRINK_IMAGES[p.key]}
+                    alt={`${p.name}`}
+                    className="productImage"
+                  />
+                  {p.stock === 0 && (
+                    <div className="soldOutOverlay">Sold Out</div>
+                  )}
+                </div>
 
-            <p>
-              {p.machineKey}. {p.name} - {formatAmount(p.price)}
-            </p>
+                <p>
+                  {p.machineKey}. {p.name} - {formatAmount(p.price)}
+                </p>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
